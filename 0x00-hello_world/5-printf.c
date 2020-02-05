3b8b9024bb32050e95b4d35b83b4e8f0a1b571b7
Jacob Ide <jacobmeide@gmail.com>
2020-02-05 19:48:15 +0000

    5,1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-main()
+/**
+ * main - Entry point
+ * usues the printf function to output a formated string
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art, \n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
 }

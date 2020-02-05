a7f92815431048c63e09c6b6804ebcf214a1e2a5
Jacob Ide <jacobmeide@gmail.com>
2020-02-05 19:42:35 +0000

    4,2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Inserts a short string of text 
+ * Inserts a short string of text
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

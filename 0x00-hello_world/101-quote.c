dae9e1bd4eceee91e579fa1e16c414014fff28e5
Jacob Ide <jacobmeide@gmail.com>
2020-02-06 00:10:27 +0000

    9,1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- *
+ * This file prints the end of the quote
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora \
 	Korpar, 2015-10-19\n");
-	return (0);
+	return (1);
 }

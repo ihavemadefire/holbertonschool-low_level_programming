44b731b53d6acc22651e1ef12ca621f34e36739b
Jacob Ide <jacobmeide@gmail.com>
2020-02-10 01:33:27 +0000

    13,0

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 
 /**
  * main - prints all unique combos of numbers between
- * between 0 and 99 using putchar
+ * between 00 01 and  98 99 using putchar
  *
  * Return: 0 if no errors
  */
@@ -11,32 +11,24 @@ int main(void)
 {
 	int i;
 	int j;
-	int k;
-	int l;
 
-	for (i = 48; i < 58; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = 48; j < 58; j++)
+		for (j = i + 1; j < 100; j++)
 		{
-			for (k = 48; k < 58; k++)
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i < 98)
 			{
-				for (l = j + 1 ; l < 58; l++)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(32);
-					putchar(k);
-					putchar(l);
-					if (i == 55 && j == 56 && k == 57)
-					{
-						putchar(10);
-					}
-					else
-					{
-						putchar(44);
-						putchar(32);
-					}
-				}
+				putchar(44);
+				putchar(32);
+			}
+			else
+			{
+				putchar(10);
 			}
 		}
 	}

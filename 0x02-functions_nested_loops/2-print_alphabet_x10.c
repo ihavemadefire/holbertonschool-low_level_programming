7d834e75193f28b6d79a019195bf21b4b5ec275d
Jacob Ide <jacobmeide@gmail.com>
2020-02-11 18:46:34 +0000

    2,3

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,7 +4,7 @@
  *
  * Return: Always 0.
  */
-int print_alphabet_X10(void)
+int print_alphabet_x10(void)
 {
 
 	char c[26] = "abcdefghijklmnopqrstuvwxyz";
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -2,5 +2,5 @@
 #define HOLBERTON_H
 int _putchar(char c);
 int print_alphabet(void);
-int print_alphabet_X10(void);
+int print_alphabet_x10(void);
 #endif

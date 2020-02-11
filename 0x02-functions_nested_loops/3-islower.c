223605e1cff7a2b26e53b2253387815575ca79b8
Jacob Ide <jacobmeide@gmail.com>
2020-02-11 20:21:28 +0000

    3,0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * islower - prints the alphabet.
+ *
+ * Return: Always 0.
+ */
+int _islower(int r)
+{
+
+	if ( r > 97 && r < 123)
+		return (0);
+	else
+		return (1);
+
+	return (0);
+}

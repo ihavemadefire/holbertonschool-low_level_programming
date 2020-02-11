960a22af8785feba3eb5ee68843d22fd0230dbd8
Jacob Ide <jacobmeide@gmail.com>
2020-02-11 21:41:28 +0000

    5,0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * print_sign - prints the alphabet.
+ *
+ * @r: is passed from main function
+ *
+ * Return: is 0 if zero, 1 if positive, -1 if negative.
+ */
+int print_sign(int r)
+{
+        if (r > 0)
+	{
+		_putchar('+');
+                return (1);
+	}
+	else if (r == 0)
+	{
+		_putchar('0');
+		return(0);
+	}
+        else
+	{
+		_putchar('-');
+		return(-1);
+	}
+}

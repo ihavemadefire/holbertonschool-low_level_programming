1cf0c32a18da9bba628601407d6f96e19eb01e34
Jacob Ide <jacobmeide@gmail.com>
2020-02-11 20:30:00 +0000

    3,2

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 /**
- * islower - prints the alphabet.
+ * _islower - prints the alphabet.
+ *
+ * @r: is passed from main function
  *
  * Return: Always 0.
  */
 int _islower(int r)
 {
-
-	if ( r > 97 && r < 123)
+	if (r > 97 && r < 123)
 		return (0);
 	else
 		return (1);
-
 }

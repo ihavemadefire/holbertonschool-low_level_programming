1e52c7a5b328730bf43bc7e9d761de761bb70107
Jacob Ide <jacobmeide@gmail.com>
2020-02-13 03:59:07 +0000

    10,1

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 /**
  * add - returns the last digit
- *
- *
+ *@a: is an integer to be added
+ *@b: is an integer to be added
  *
  * Return: is 0 if zero, 1 if positive, -1 if negative.
  */

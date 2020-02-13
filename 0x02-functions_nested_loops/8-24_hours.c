117b0a4af0203cda59757191f96ac867cfe3eb0e
Jacob Ide <jacobmeide@gmail.com>
2020-02-13 02:24:04 +0000

    0,1

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,25 @@
 #include "holberton.h"
 /**
- * print_last_digit - returns the last digit
+ * jack_bauer - returns the last digit
  *
- * @r: is passed from main function
+ *
  *
  * Return: is 0 if zero, 1 if positive, -1 if negative.
  */
 void jack_bauer(void)
 {
-        int i;
+	int i;
 	int j;
-      	for (i = 0; i < 24;i++)
+
+	for (i = 0; i < 24; i++)
 	{
 		for (j = 0; j <= 59 ; j++)
 		{
-			_putchar((i/10)+'0');
-			_putchar((i%10)+'0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((j/10)+'0');
-			_putchar((j%10)+'0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar(10);
 		}
 	}

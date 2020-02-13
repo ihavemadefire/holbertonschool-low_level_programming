0876443acb0bae36ddeca958be2071ed752fb8a0
Jacob Ide <jacobmeide@gmail.com>
2020-02-13 03:35:54 +0000

    9,0

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+/**
+ * void times_table(void) - returns the last digit
+ *
+ *
+ *
+ * Return: is 0 if zero, 1 if positive, -1 if negative.
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0');
+		for(j = 1; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			k = j * i;
+			if((k/10) == 0)
+			{
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar((k / 10) + '0');
+				_putchar((k % 10) + '0');
+			}
+			if (j == 9)
+				_putchar('\n');
+		}
+	}
+}

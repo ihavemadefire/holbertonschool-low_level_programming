eb357fc86c4f8f2fc9cffe72d1c8c4008ff2dc4b
Jacob Ide <jacobmeide@gmail.com>
2020-02-13 16:20:45 +0000

    0,0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * mul - prints the alphabet.
+ *
+ * @a: is passed from main function
+ * @b: is passed from main function
+ * Return: Always 0.
+ */
+int mul(int a,  int b)
+{
+	int r;
+
+	r = (a * b);
+	return (r);
+}

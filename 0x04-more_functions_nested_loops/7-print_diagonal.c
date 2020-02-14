cbda0ecabbf2fb49c8df1d050d0f2e2b8b7d9c74
Jacob Ide <jacobmeide@gmail.com>
2020-02-14 00:04:36 +0000

    7,0

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_line - is a function that prints numbers
+ *
+ *@n: is passed from main funtion
+ */
+
+void print_diagonal(int n)
+{
+	int r;
+	int s;
+
+	if (n <= 0)
+		_putchar(10);
+	else
+	{
+		for (r = 0; r<=n ; r++)
+		{
+			for (s = 0; s < r; s++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar(10);
+		}
+	}
+}

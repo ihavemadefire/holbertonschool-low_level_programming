ea80e85c7a6fafa415741364b45c611e6648812e
Jacob Ide <jacobmeide@gmail.com>
2020-02-14 01:10:40 +0000

    9,0

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
+/**
+ * main - FizzBuzz
+ */
 
 int main(void)
 {
 	int i;
 
-	for(i = 0; i <101 ; i++)
+	for (i = 1; i < 101 ; i++)
 	{
 		if (((i % 3) == 0) && ((i % 5) == 0))
-				  {
-					  printf("FizzBuzz ");
-				  }
+		{
+			printf("FizzBuzz ");
+		}
 		else if (i % 3 == 0)
 			 printf("Fizz ");
 		else if (i % 5 == 0)

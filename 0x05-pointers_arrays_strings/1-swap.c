92497c90c6dc485d4f74c6f91a079d59748f34e2
Jacob Ide <jacobmeide@gmail.com>
2020-02-17 16:29:06 +0000

    1,1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,8 @@
 /**
  * swap_int - function that takes a pointer to an int as
  *      parameter and updates the value it points to to 98
- * @n: Passed pointer
+ * @a: Passed pointer
+ * @b: Passed pointer
  *
  */
 void swap_int(int *a, int *b)
@@ -11,5 +12,5 @@ void swap_int(int *a, int *b)
 	int i = *a;
 	int j = *b;
 	*a = j;
-        *b = i;
+	*b = i;
 }

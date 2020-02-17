2bd62a99e01eefca1096858316954321fd6d96fc
Jacob Ide <jacobmeide@gmail.com>
2020-02-17 02:59:04 +0000

    13,0

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main -  prints natural numbers
+ *
+ *return: always returns 0.
+**/
+int main (void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 1025 ; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			j += i;
+	}
+	printf("%d\n",j);
+	return (0);
+}

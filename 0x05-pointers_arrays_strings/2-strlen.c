a9a086de20d24e6e18f3c0d74d58b687db0a114f
Jacob Ide <jacobmeide@gmail.com>
2020-02-17 18:27:44 +0000

    2,0

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strlen - function that takes a pointer and returns the length of the
+ *               string
+ * @s: Passed pointer
+ *
+ *Return - returns length of the passed string
+ */
+int _strlen(char *s)
+{
+	int i =0;
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}

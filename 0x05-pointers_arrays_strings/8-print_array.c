1ac52c0c70c8bc75a351f559f36260bf5bb36bbe
Jacob Ide <jacobmeide@gmail.com>
2020-02-18 17:00:46 +0000

    8,0

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - function that takes a pointer and returns the length of the
+ *               string
+ * @a: array to be scanned
+ * @n: number of places to be printed
+ *
+ * Return: No return value for puts_half
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	printf("%d", *a);
+	for (i = 1; i < n; i++)
+	{
+		printf(", %d", *(a + i));
+	}
+	printf("\n");
+}

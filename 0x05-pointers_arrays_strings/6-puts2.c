95f47124b22cbdd445eb67b74400d6183699728e
Jacob Ide <jacobmeide@gmail.com>
2020-02-18 15:47:23 +0000

    6,1

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,13 +6,29 @@
  *
  *
  */
+int string_length(char *s);
 void puts2(char *s)
 {
-	while (*s != '\0')
-	{
-		_putchar(*s);
-		s++;
-		s++;
-	}
+	int i;
+	int j;
+
+	j = string_length(s);
+	for (i = 0;i < j; i+=2)
+		_putchar(*(s + i));
 	_putchar(10);
+}
+/**
+ * string_length - returns length of string
+ *
+ * @s: Passed pointer
+ *
+ * Return: returns length of string
+ */
+int string_length(char *s)
+{
+        int c = 0;
+
+        while (*(s + c) != '\0')
+                c++;
+        return (c);
 }

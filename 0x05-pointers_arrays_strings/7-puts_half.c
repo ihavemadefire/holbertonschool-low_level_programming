a5cf7edac32f12bbff1735777eb7aff9d5314009
Jacob Ide <jacobmeide@gmail.com>
2020-02-18 16:23:34 +0000

    7,1

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,37 +1,43 @@
 #include "holberton.h"
-#include <stdio.h>
+
 /**
  * puts_half - function that takes a pointer and returns the length of the
  *               string
  * @s: Passed pointer
  *
- *
+ * Return: No return value for puts_half
  */
-int string_length(char*);
+int string_length(char *s);
 void puts_half(char *s)
 {
 	int length;
 	int k;
 
-	length =string_length(s);
+	length = string_length(s);
 	if (length % 2 == 0)
 	{
-		for(k = (length / 2); k < length; k++)
+		for (k = (length / 2); k < length; k++)
 			_putchar(*(s + k));
 	}
 	else
 	{
-		for(k = ((length - 1) / 2); k < length; k++)
+		for (k = ((length - 1) / 2); k < length; k++)
 			_putchar(*(s + k));
 	}
 	_putchar(10);
 }
-
-int string_length(char *ch)
+/**
+ * string_length - returns length of string
+ *
+ * @s: Passed pointer
+ *
+ * Return: returns length of string
+ */
+int string_length(char *s)
 {
 	int c = 0;
 
-	while( *(ch + c) != '\0' )
+	while (*(s + c) != '\0')
 		c++;
-	return c;
+	return (c);
 }

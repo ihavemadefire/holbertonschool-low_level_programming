6c984ce9900432f6327c3f4814294bb8de942d49
Jacob Ide <jacobmeide@gmail.com>
2020-02-18 15:12:00 +0000

    5,1

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,41 +2,39 @@
 /**
  * rev_string - function that reverses a string
  *
+ * string_length - length of string
+ *
  * @s: Passed pointer
  *
- *
+ * Return: Reutns lenth of passed pointer
  */
-int string_length(char*);
+int string_length(char *s);
 void rev_string(char *s)
 {
+	int length, c;
+	char *start, *end, temp;
+
+	length = string_length(s);
+	start  = s;
+	end    = s;
+
+	for (c = 0; c < length - 1; c++)
+		end++;
+
+	for (c = 0; c < length / 2; c++)
 	{
-		int length, c;
-		char *start, *end, temp;
-
-	       length = string_length(s);
-	       start  = s;
-	       end    = s;
-
-	       for (c = 0; c < length - 1; c++)
-			end++;
-
-	       for (c = 0; c < length/2; c++)
-		{
-			temp   = *end;
-			*end   = *begin;
-			*begin = temp;
-
-			begin++;
-			end--;
-		}
+		temp   = *end;
+		*end   = *start;
+		*start = temp;
+		start++;
+		end--;
 	}
 }
-
-int string_length(char *pointer)
+int string_length(char *s)
 {
 	int c = 0;
 
-	while( *(pointer + c) != '\0' )
+	while (*(s + c) != '\0')
 		c++;
-	return c;
+	return (c);
 }

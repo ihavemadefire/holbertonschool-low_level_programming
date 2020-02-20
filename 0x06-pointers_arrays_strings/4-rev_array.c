dd9cbe88d41df26f15e6048d0d35c4bce28f5462
Jacob Ide <jacobmeide@gmail.com>
2020-02-20 22:42:00 +0000

    4,0

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ *reverse_array - returns a concatednated char pointer
+ *@a: passed pointer to be compared
+ *@n: passed pointer to be compared
+ *
+ *
+ *Return: copied string
+ */
+
+void reverse_array(int *a, int n)
+{
+
+	int i;
+	int tmp,*beg, *end;
+	beg = a;
+	end = a;
+
+	for (i = 0; i < n - 1; i++)
+		end++;
+
+	for (i = 0; i < n / 2 ;i++)
+	{
+		tmp = *end;
+		*end = *beg;
+		*beg = tmp;
+		beg++;
+		end--;
+	}
+}

34820e1889126b6adf69f14501e9dd30a412ba9f
Jacob Ide <jacobmeide@gmail.com>
2020-02-20 22:44:18 +0000

    4,1

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,14 +12,15 @@ void reverse_array(int *a, int n)
 {
 
 	int i;
-	int tmp,*beg, *end;
+	int tmp, *beg, *end;
+
 	beg = a;
 	end = a;
 
 	for (i = 0; i < n - 1; i++)
 		end++;
 
-	for (i = 0; i < n / 2 ;i++)
+	for (i = 0; i < n / 2 ; i++)
 	{
 		tmp = *end;
 		*end = *beg;

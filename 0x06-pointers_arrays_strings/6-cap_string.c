9454dec19f974dacccacedc6d8da815f53ec4aea
Jacob Ide <jacobmeide@gmail.com>
2020-02-21 03:11:18 +0000

    6,0

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * cap_string - returns a concatednated char pointer
+ *@s: passed pointer to be compared
+ *
+ *
+ *
+ *Return: copied string
+ */
+
+char *cap_string(char *s)
+{
+	int i, j;
+	char not[] = {' ', '\t', '\n', ',', ';', '.',
+		      '!', '?', '"', '(', ')', '{', '{'};
+
+	if (*s >= 97 && *s <= 122)
+		s -= 32;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 12; j++)
+		{
+			if (not[j] == s[i])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+	return (s);
+}

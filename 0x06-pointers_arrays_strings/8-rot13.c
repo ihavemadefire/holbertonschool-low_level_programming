45cfcef5e5726b8619a7679b8d0d60deddd6acfc
Jacob Ide <jacobmeide@gmail.com>
2020-02-21 05:44:38 +0000

    8,0

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * rot13 - returns a concatednated char pointer
+ *@s: passed pointer to be compared
+ *
+ *
+ *
+ *Return: copied string
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char rl[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char sl[52] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j <= 52; j++)
+		{
+			if (s[i] == rl[j])
+				s[i] = sl[j];
+		}
+	}
+	return (s);
+}

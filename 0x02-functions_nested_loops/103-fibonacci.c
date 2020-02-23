28edff19cbcfa22bd0c5d795e185ad8cac9cd530
Jacob Ide <jacobmeide@gmail.com>
2020-02-23 02:50:13 +0000

    15,1

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,17 +6,17 @@
  **/
 int main(void)
 {
-	long j = 1;
-	long k = 2;
+	long j = 0;
+	long k = 1;
 	long l = 0;
 	long m = 0;
 
-	while (k < 40000000)
+	while (k < 4000000)
 	{
 		l = j + k;
 		j = k;
 		k = l;
-		if (l % 2 ==0)
+		if (l % 2 == 0)
 			m += l;
 	}
 	printf("%li\n", m);

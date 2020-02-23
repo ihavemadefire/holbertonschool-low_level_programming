a2bc729eb86ac14217b5ee16c2cefa8269c45101
Jacob Ide <jacobmeide@gmail.com>
2020-02-23 00:46:52 +0000

    9,0

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,11 @@
-#include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - Entry point
- * This file prints the end of the quote
- * Return: Always 1 (Success)
+ *main - prints text without using write
  */
 int main(void)
 {
-	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora\
-	Korpar, 2015-10-19\n");
+	(write(1, "and that piece of art is useful\
+	\" - Dora Korpar, 2015-10-19\n", 60));
+
 	return (1);
 }

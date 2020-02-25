138979f1112c9ca55d85a3660e118642ebd7bb64
Jacob Ide <jacobmeide@gmail.com>
2020-02-25 01:12:54 +0000

    4,0

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * *_strpbrk - find first instance of a set of character
+ *
+ * @s: pointer array to be searched
+ * @accept: pointer array with char to be searched
+ * Return:  pointer to array
+ */
+char *_strchr(char *s, char c);
+char *_strpbrk(char *s, char *accept)
+{
+	for (; *s; s++)
+	{
+		if (_strchr(accept, *s) != 0)
+			return (s);
+	}
+	return (0);
+}
+/**
+ **_strchr - locates a character in a string
+ *@s: String to be searched
+ *@c: Charater to be searched for
+ *
+ *
+ *Return: memory address
+ */
+char *_strchr(char *s, char c)
+{
+        while (*s != '\0')
+        {
+                if (*s == c)
+                        return (s);
+                s++;
+        }
+        if (c == '\0')
+                return (s);
+        s = 0;
+        return (s);                                                                                                                              }

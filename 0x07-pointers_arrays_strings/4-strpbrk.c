4b3d9eeab0a4e100c335747a4ffb9d2b19c9f4b6
Jacob Ide <jacobmeide@gmail.com>
2020-02-25 01:21:14 +0000

    4,1

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -3,8 +3,10 @@
 /**
  * *_strpbrk - find first instance of a set of character
  *
+ * *_strchr - locates a character in a string
+ * @c: Charater to be searched for
  * @s: pointer array to be searched
- * @accept: pointer array with char to be searched
+ *
  * Return:  pointer to array
  */
 char *_strchr(char *s, char c);
@@ -22,18 +24,18 @@ char *_strpbrk(char *s, char *accept)
  *@s: String to be searched
  *@c: Charater to be searched for
  *
- *
  *Return: memory address
  */
 char *_strchr(char *s, char c)
 {
-        while (*s != '\0')
-        {
-                if (*s == c)
-                        return (s);
-                s++;
-        }
-        if (c == '\0')
-                return (s);
-        s = 0;
-        return (s);                                                                                                                              }
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (c == '\0')
+		return (s);
+	s = 0;
+	return (s);
+}

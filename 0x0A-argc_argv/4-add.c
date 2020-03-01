7b6945c62573dba9ed943ca7e1a19afa59e23a10
Jacob Ide <jacobmeide@gmail.com>
2020-03-01 05:46:15 +0000

    4,0

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Prints the name of the function
+ *@argc: not used in this function
+ *@argv: list of passed arguments from the cl
+ * Return: Always returns 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	if (argc <=1)
+		printf("0\n");
+	for (i = 1; i < argc; i++)
+	{
+		sum += (atoi(argv[i]));
+		for (j = 0; argv[i][j]; j++)
+		{
+			if (!(isdigit(argv[i][j])))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}

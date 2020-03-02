22183bd7e6b80470ea87ee9601f85adafdf3f16c
Jacob Ide <jacobmeide@gmail.com>
2020-03-02 02:24:03 +0000

    5,4

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,6 +17,11 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+	if (!(atoi(argv[1])))
+	{
+		printf("Error\n");
+		return (1);
+	}
 	for (i = 0; argv[1][i] ; i++)
 	{
 		if (!(isdigit(argv[1][i])))

ab9781ce292caa22e8e88b09e1746225ae5a0cc2
Jacob Ide <jacobmeide@gmail.com>
2020-03-04 01:48:08 +0000

    3,0

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * **alloc_grid - function to allocate memory to build grid
+ * @width: width of grid
+ * @height: height of grid
+ * Return: grid of 0s
+ */
+
+int **alloc_grid(int width, int height)

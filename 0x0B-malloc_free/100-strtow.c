faa7e2747850bf4ed15a91e4806409cbac5f777e
Jacob Ide <jacobmeide@gmail.com>
2020-03-04 05:29:12 +0000

    6,0

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * strtow - convert command line text args to strings
+ * @str: passed string to be parsed
+ *
+ * Return: arguments as line separated strings
+ */
+
+char **strtow(char *str)
+{
+	int i, j;
+	char *ptr;
+
+	if (str == "" || str == NULL)
+	{
+		return (NULL);
+	}
+}

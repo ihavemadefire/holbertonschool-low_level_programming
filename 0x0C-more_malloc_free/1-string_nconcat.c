da0ead6af5cfbcc48b85ac90e0ba1b1301303e08
Jacob Ide <jacobmeide@gmail.com>
2020-03-05 23:03:02 +0000

    1,1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,9 @@ int _strlen(char *s);
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ch;
-	unsigned int sizeOf1, sizeOf2, i;
+	int i;
+	int k = n;
+	int sizeOf1, sizeOf2;
 
 	if (s1 == NULL) /*if s1 is NULL, assign empty*/
 		s1 = "";
@@ -21,23 +23,23 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	sizeOf1 = _strlen(s1); /* length of s1 */
 	sizeOf2 = _strlen(s2); /* length of s2 */
 
-	ch = malloc((sizeOf1 + sizeOf2) + 1 * sizeof(char));
+	if (k >= sizeOf2)
+	{
+		k = sizeOf2;
+		ch = malloc(sizeof(char) * (sizeOf1 + sizeOf2 + 1));
+	}
+	else
+	{
+		ch = malloc((sizeOf1 + sizeOf2) + 1 * sizeof(char));
+	}
 	if (ch == NULL)
 	{
 		return (NULL);
 	}
 	for (i = 0; s1[i]; i++) /* copy string one to ch pointer*/
 		ch[i] = s1[i];
-	if (sizeOf2 >= n)
-	{
-		for (i = 0; s2[i]; i++)/* copy string to to end of ch */
-			ch[sizeOf1 + i] = s2[i];
-	}
-	else
-	{
-		for (i = 0; i < n ; i++)/* copy string to to end of ch */
-			ch[sizeOf1 + i] = s2[i];
-	}
+	for (i = 0; i < k; i++)/* copy string to to end of ch */
+		ch[sizeOf1 + i] = s2[i];
 	ch[sizeOf1 + sizeOf2] = '\0';
 	return (ch);
 }

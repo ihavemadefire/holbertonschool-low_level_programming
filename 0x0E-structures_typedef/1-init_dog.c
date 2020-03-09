f2537f5d6e481a8a487fd34b3a13ac0310502bfb
Jacob Ide <jacobmeide@gmail.com>
2020-03-09 19:44:57 +0000

    1,0

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - assigns value to dog
+ * @d: Dog Object
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ **/
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
+}

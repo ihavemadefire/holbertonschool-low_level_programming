310917950dafeca588147ddb74646a391e28328f
Jacob Ide <jacobmeide@gmail.com>
2020-03-09 18:51:15 +0000

    0,1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef dog_h
 #define dog_h
+/**
+ * struct dog - dog attributes
+ * @name: Dog Name
+ * @owner: owner name
+ * @age: Age of dog
+ * Description: Data type to store doggie-flavored info
+ */
 struct dog
 {
 	char *name;

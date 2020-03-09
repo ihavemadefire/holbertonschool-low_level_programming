f4e1786f588b68d204b23390081343d503c928a8
Jacob Ide <jacobmeide@gmail.com>
2020-03-09 17:54:05 +0000

    4,0

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,7 @@
+#ifndef object_like_macro_h
+#define object_like_macro_h
+#define PI 3.14159265359
+#define SIZE 1024
+#define ABS(num) ((num > 0) ? num : -(num))
+#define SUM(x, y) ((x) + (y))
+#endif

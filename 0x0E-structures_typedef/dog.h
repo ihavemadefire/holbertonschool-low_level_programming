5c862915456134deaa9859939ac04b2a64635c38
Jacob Ide <jacobmeide@gmail.com>
2020-03-10 02:36:44 +0000

    header

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,13 +7,17 @@
  * @age: Age of dog
  * Description: Data type to store doggie-flavored info
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	char  *owner;
 	float age;
-};
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 #endif

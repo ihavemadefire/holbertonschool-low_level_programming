7c86217e8bfa1c33b3b7357bc98e1ec218f90a69
Jacob Ide <jacobmeide@gmail.com>
2020-03-19 17:47:06 +0000

    0,1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "lists.h"
-/*
+/**
  * print_list - Prints elements of a linked list
  * @h: list to be printer
  * Return: number of elements
@@ -8,7 +8,7 @@
 
 size_t print_list(const list_t *h)
 {
-	const list_t * current = h;
+	const list_t *current = h;
 	int i = 0;
 
 	while (current != NULL)
@@ -19,7 +19,7 @@ size_t print_list(const list_t *h)
 		}
 		else
 		{
-			printf("[%d] %s\n",current->len, current->str);
+			printf("[%d] %s\n", current->len, current->str);
 		}
 		i++;
 		current = current->next;

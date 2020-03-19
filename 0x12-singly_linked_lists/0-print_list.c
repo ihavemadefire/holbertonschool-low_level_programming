4909488308f4d745a3b6bc7f87186b9596bda2ef
Jacob Ide <jacobmeide@gmail.com>
2020-03-19 17:41:09 +0000

    0,0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "lists.h"
+/*
+ * print_list - Prints elements of a linked list
+ * @h: list to be printer
+ * Return: number of elements
+*/
+
+size_t print_list(const list_t *h)
+{
+	const list_t * current = h;
+	int i = 0;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] Null\n");
+		}
+		else
+		{
+			printf("[%d] %s\n",current->len, current->str);
+		}
+		i++;
+		current = current->next;
+	}
+	return (i);
+}

d08a3ec18e5d1a2162f3e9aa621a04b38cd5095f
Jacob Ide <jacobmeide@gmail.com>
2020-03-20 00:37:24 +0000

    6,9

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,13 @@
-;; -----------------------------------------------------------------------------
 ;;  Writes "Hello, Holberton" to the console using a C library. Runs on Linux.
 ;;
-;;
-;;  ----------------------------------------------------------------------------
+
 segment .data
 hello db "Hello, Holberton", 0xa, 0
 
 segment .text
 	global	main
 	extern	printf
-	extern	exit
 main:
 	mov	rax, 0
 	mov	rdi, hello
 	call	printf
-	call	exit

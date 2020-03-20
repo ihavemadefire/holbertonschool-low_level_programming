d918ef7191ba2fdb25eda08e2658392058e6bc37
Jacob Ide <jacobmeide@gmail.com>
2020-03-20 00:12:42 +0000

    6,5

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,18 @@
-;; -----------------------------------------------------------------------------------------
+;; -----------------------------------------------------------------------------
 ;;  Writes "Hello, Holberton" to the console using a C library. Runs on Linux.
 ;;
 ;;
-;;  ----------------------------------------------------------------------------------------
+;;  ----------------------------------------------------------------------------
 
 	          global    main
 	          extern    printf
 		  extern    exit
 
 	          section   .text
-main:				; This is called by the C library startup code
+main:
 		  mov       rax, 0
-		  mov       rdi, hello ; First integer (or pointer) argument in rdi
+		  mov       rdi, hello
 	          call      printf
 	          call      exit
 message:
-hello	          db        "Hello, Holberton", 0xa, 0 ; Note strings must be terminated with 0 in C
+hello	          db        "Hello, Holberton", 0xa, 0

ca50a275b7ae6eddd9b6ca1fc8939666e1d16284
Jacob Ide <jacobmeide@gmail.com>
2020-03-23 17:19:52 +0000

    0,0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - Prints elements of a linked list
+ * @h: list to be printer
+ * Return: number of elements
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *current = h;
+	int i = 0;
+
+	while (current != NULL)
+	{
+		printf("%d\n", current->n);
+		i++;
+		current = current->next;
+	}
+	return (i);
+}

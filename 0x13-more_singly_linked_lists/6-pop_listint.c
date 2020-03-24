48ab6671143f6a42d2cc535511def514a636290b
Jacob Ide <jacobmeide@gmail.com>
2020-03-24 07:24:00 +0000

    6,1

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * pop_listint - removes first element of a linked list
+ * @head: pointer to pointer of LL
+ * Return: 0 if list is empty; otherwise, pop contents
+ */
 int pop_listint(listint_t **head)
 {
 	listint_t *pop;

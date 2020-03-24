ad40fba1bfcf8bf1d34e9b29024cd3351c76b347
Jacob Ide <jacobmeide@gmail.com>
2020-03-24 17:53:38 +0000

    11,0

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * reverse_listint - Prints elements of a linked list
+ * @head: list to be printer
+ * Return: number of elements
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *current;
+	listint_t *end = NULL;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (current != NULL)
+	{
+		current = (*head)->next;
+		(*head)->next = end;
+		end = *head;
+		*head = current;
+	}
+	*head = end;
+	return (*head);
+}

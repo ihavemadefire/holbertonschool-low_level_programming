ac26899232f7b46d86fab2473c4f9f148395037c
Jacob Ide <jacobmeide@gmail.com>
2020-03-24 07:44:48 +0000

    7,2

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,17 +10,22 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	listint_t *tmp = NULL;
-	unsigned int i;
+	listint_t *tmp;
+	unsigned int i = 0;
 
 	if (head == NULL)
 	{
 		return (NULL);
 	}
-	tmp = head;
-	for (i = 0; i < index; i++)
+	while (head != NULL)
 	{
+		if (i == index)
+		{
+			tmp = head;
+			return (tmp);
+		}
+		i++;
 		tmp = tmp->next;
 	}
-	return (tmp);
+	return (NULL);
 }

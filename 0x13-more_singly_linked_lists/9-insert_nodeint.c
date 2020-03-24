7952d52835b772473304cd6b38204a213467bc1d
Jacob Ide <jacobmeide@gmail.com>
2020-03-24 16:11:44 +0000

    9,1

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -28,6 +28,12 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = tmp;
 		return (new);
 	}
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	for (i = 0; i < (idx - 1); i++)
 	{
 		tmp = tmp->next;

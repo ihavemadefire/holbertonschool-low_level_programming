a34270e4629bbee2be00591e76705d69ff5bb8e9
Jacob Ide <jacobmeide@gmail.com>
2020-03-26 15:38:35 +0000

    3,0

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * set_bit -sets bit at index
+ * @n: int to be manipulated
+ * @index: index at which to manipulate
+ * Return: 1 on success; -1 if failure.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+
+	*n =  *n | (1 << index);
+	return (1);
+}

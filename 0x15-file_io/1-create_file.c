cd2f883b47f204551c78607763caaddfd266f98f
Jacob Ide <jacobmeide@gmail.com>
2020-03-31 20:33:09 +0000

    1,1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,6 +17,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)/*Check for null filename */
 		return (-1);
+	if (text_content == NULL)
+		text_content = "";
 	for (len = 0; text_content[len]; len++)
 		;
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);

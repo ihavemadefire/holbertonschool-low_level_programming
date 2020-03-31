fa3d437bad704704668b9cf2cd203b079b576505
Jacob Ide <jacobmeide@gmail.com>
2020-03-31 20:28:00 +0000

    1,2,

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,23 +13,16 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len, i;
-	char *buf;
+	int o, w, len;
 
 	if (filename == NULL)/*Check for null filename */
 		return (-1);
 	for (len = 0; text_content[len]; len++)
 		;
-	buf = malloc(sizeof(char) * len);
-	if (buf ==  NULL)
-		return (-1);
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	for (i = 0; i < len; i++)
-		buf[i] = text_content[i];
-	w = write(o, buf, len);
+	w = write(o, text_content, len);
 	if (o == -1 || w == -1)
 	{
-		free(buf);
 		return (-1);
 	}
 	close(o);

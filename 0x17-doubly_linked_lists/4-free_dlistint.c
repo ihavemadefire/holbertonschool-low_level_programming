d4a8c646b57f9f2a30face979ccc0d9c1b1ff6c7
Jacob Ide <jacobmeide@gmail.com>
2020-05-01 22:32:02 +0000

    4,0

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_dlist_int - frees each element of a linked list
+ * @head: pointer to head
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		free(temp);
+		head = head->next;
+	}
+}

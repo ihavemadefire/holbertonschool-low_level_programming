3edc8619b7cb7ccad2a73f73c30221701fa22e99
Jacob Ide <jacobmeide@gmail.com>
2020-05-02 18:02:13 +0000

    0,1

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -8,8 +8,8 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *current = rewnd(h);
 	size_t length = 0;
+	const dlistint_t *current = h;
 
 	while (current != NULL)
 	{
@@ -19,20 +19,4 @@ size_t print_dlistint(const dlistint_t *h)
 
 	}
 	return (length);
-}
-/**
- * rewnd - Prints all the elements of a list
- * @h: address of list of elements
- * Return: pointer to head
- */
-
-const dlistint_t *rewnd(const dlistint_t *h)
-{
-	const dlistint_t *temp = h;
-
-	while (temp->prev != NULL)
-	{
-		temp = temp->prev;
-	}
-	return (temp);
 }

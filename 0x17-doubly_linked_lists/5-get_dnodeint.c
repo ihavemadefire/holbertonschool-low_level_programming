7bd93c439159a503ea4c964a8c972283486ffa27
Jacob Ide <jacobmeide@gmail.com>
2020-05-03 01:53:26 +0000

    5,1

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -11,6 +11,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	dlistint_t *node;
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
 	node = head;
 	while (i != index)
 	{

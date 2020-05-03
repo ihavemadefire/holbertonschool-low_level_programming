40ae1de9e011dfcfadda7e164be09d683108df95
Jacob Ide <jacobmeide@gmail.com>
2020-05-03 17:12:04 +0000

    8,3

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -23,8 +23,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (index == 0)/*check for 0 index*/
 	{
 		tmp->prev = NULL;
-		tmp->next = tmp->next->next;
 		*head = tmp->next;
+		tmp->next = tmp->next->next;
 		free(tmp);
 		return (1);
 	}

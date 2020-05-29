1ee41f29428b0eef8b7daca5877fcf66c6daa8ed
Jacob Ide <jacobmeide@gmail.com>
2020-05-29 18:26:03 +0000

    3,2

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,4 @@
 #include "hash_tables.h"
-
 /**
  * hash_table_set -
  * @ht: table to be set
@@ -18,12 +17,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	dupvalue = (char *)strdup(value);
 
 	index = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[index]->key == NULL)
+	if (ht->array[index] == NULL)
 	{
 		/*for an empty block*/
-		ht->array[index]->key = dupkey;
-		ht->array[index]->value = dupvalue;
-		ht->array[index]->next = NULL;
+		new = malloc(sizeof(hash_node_t));
+		if (new == NULL)
+		{
+			return (1);
+		}
+		ht->array[index] = new;
+		new->key = dupkey;
+		new->value = dupvalue;
+		new->next = NULL;
 		return (0);
 	}
 	else if (strcmp(ht->array[index]->key, key))
@@ -39,7 +44,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		new = malloc(sizeof(hash_node_t));
 		if (new == NULL)
 		{
-			return (-1);
+			return (1);
 		}
 		new->value = dupvalue;
 		new->key = dupkey;

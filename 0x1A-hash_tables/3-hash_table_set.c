feb566b4b2d20b85cc3ff23c922d46ccef65964f
Jacob Ide <jacobmeide@gmail.com>
2020-05-30 00:50:44 +0000

    3,9

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new, *temp;
 	char *dupkey, *dupvalue;
 
-	if (key == NULL || ht == NULL || key == "")
+	if (key == NULL || ht == NULL || strcmp(key, "") == 0)
 		return (0);
 	dupkey = (char *)strdup(key);
 	dupvalue = (char *)strdup(value);

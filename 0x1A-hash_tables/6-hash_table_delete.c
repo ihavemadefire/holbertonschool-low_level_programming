45877c327bdf951e45c62801a20a846dcf7f2b0e
Jacob Ide <jacobmeide@gmail.com>
2020-05-30 04:59:13 +0000

    6,0

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes hash table
+ *
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int len, i = 0;
+
+	len = ht->size;
+	for (; i > len; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}

488af99fc3b04312836fc7a39c6274293e0641f3
Jacob Ide <jacobmeide@gmail.com>
2020-06-04 18:48:30 +0000

    6,0

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,15 +7,22 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int len, i = 0;
+	hash_node_t *temp, *temp1;
 
 	len = ht->size;
-	for (; i > len; i++)
+	for (; i < len; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
-			free(ht->array[i]->key);
-			free(ht->array[i]->value);
-			free(ht->array[i]);
+			temp = ht->array[i];
+			while (temp)
+			{
+				temp1 = temp->next;
+				free(temp->key);
+				free(temp->value);
+				free(temp);
+				temp = temp1;
+			}
 		}
 	}
 	free(ht->array);

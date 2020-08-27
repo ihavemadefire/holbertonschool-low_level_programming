59081190e565a70ba5cf1fd0d5d3973438c00cde
Jacob Ide <jacobmeide@gmail.com>
2020-08-27 05:10:43 +0000

    0

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -31,7 +31,7 @@ int jump_search(int *array, size_t size, int value)
 		right += sqrt(size1);
 		printf("Value checked array[%d] = [%d]\n", left, array[left]);
 	}
-	printf("Value found between indexes [%d] = [%d]\n", left, right);
+	printf("Value found between indexes [%d] and [%d]\n", left, right);
 	for (i = left; i <= right; i++)
 	{
 		if (i < size1)
